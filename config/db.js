987a5a3534617f5c8f5bc4ff509c43c8cbcc7c1a
Vikram Swami <vikramswami62421@gmail.com>
2026-04-21 22:45:33 +0530

    Added SSL + DB config fix

--- config/db.js
+++ config/db.js
@@ -8,6 +8,13 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+
+  ssl: {
+    rejectUnauthorized: false
+  },
+
+  connectTimeout: 20000
 });
 
 db.connect((err) => {
